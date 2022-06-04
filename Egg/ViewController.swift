571aebaaf426113f8a155f1f838e8f4b0884a082
Victor Andre de Paula e Silva <victorandre.ps@gmail.com>
2022-06-04 13:38:50 +1000

    Initial Commit

---
+++ Egg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Egg
+//
+//  Created by Victor Andre de Paula e Silva on 4/6/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
